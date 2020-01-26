0f753013c65607ba2f2fe87c8c2bfb18c05eda44
vjpal3 <vrishalipal@yahoo.com>
2020-01-25 19:19:56 -0500

    Use axios and post data to Firebase

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,7 @@ import Burger from './../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import axios from '../../axios-orders';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -65,7 +66,27 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert('You Continue!');
+    //alert('You Continue!');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice, // recalculate it on the server in real app
+      customer: {
+        name: 'Vrishali Pal',
+        address: {
+          street: 'TestStreet 1',
+          zipCode: '01234',
+          country: 'USA',
+          email: 'test@test.com'
+        },
+        deliveryMethod: 'fastest'
+      }
+    };
+    axios
+      .post('/orders.json', order)
+      .then(response => {
+        console.log(response);
+      })
+      .catch(error => console.log(error));
   };
 
   render() {
