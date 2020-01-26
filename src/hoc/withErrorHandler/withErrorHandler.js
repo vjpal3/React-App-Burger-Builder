143b80bc6f18e42db458e0c370a12f0e57137ed2
vjpal3 <vrishalipal@yahoo.com>
2020-01-26 00:18:11 -0500

    Implement global error handler

---
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -0,0 +1,52 @@
+import React, { Component } from 'react';
+import Modal from '../../components/UI/Modal/Modal';
+
+const withErrorHandler = (WrappedComponent, axios) => {
+  return class extends Component {
+    state = {
+      error: null
+    };
+
+    componentDidMount() {
+      //Clear if any previous errors
+      axios.interceptors.request.use(req => {
+        this.setState({ error: null });
+        return req;
+      });
+
+      //set up new errors, if any
+      axios.interceptors.response.use(
+        res => res,
+        error => {
+          //error obj returned from Firebase has a message property.
+          this.setState({ error: error });
+        }
+      );
+    }
+
+    errorConfirmedHandler = () => {
+      this.setState({
+        error: null
+      });
+    };
+
+    render() {
+      return (
+        <>
+          <Modal
+            show={this.state.error}
+            modalClosed={this.errorConfirmedHandler}
+          >
+            {
+              //error.message returned from Firebase
+            }
+            {this.state.error ? this.state.error.message : null}
+          </Modal>
+          <WrappedComponent {...this.props} />
+        </>
+      );
+    }
+  };
+};
+
+export default withErrorHandler;
