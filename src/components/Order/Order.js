e10a23c73d2955690c5ab6627941d732f9ec9c08
vjpal3 <vrishalipal@yahoo.com>
2020-02-05 00:03:39 -0500

    Fetch and Show orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -2,10 +2,40 @@ import React from 'react';
 import classes from './Order.module.css';
 
 const order = props => {
+  const ingredients = [];
+
+  for (let igName in props.ingredients) {
+    ingredients.push({
+      name: igName,
+      quantity: props.ingredients[igName]
+    });
+  }
+
+  const ingredientOutput = ingredients.map(ingredient => {
+    return (
+      <span
+        style={{
+          textTransform: 'capitalize',
+          display: 'inline-block',
+          margin: '0 8px',
+          border: '1px solid #ccc',
+          padding: '5px'
+        }}
+        key={ingredient.name}
+      >
+        {ingredient.name} ({ingredient.quantity})
+      </span>
+    );
+  });
   return (
     <div className={classes.Order}>
-      <p>Ingredients: </p>
-      <p>Price: </p>
+      <p>
+        Ingredients:
+        {ingredientOutput}{' '}
+      </p>
+      <p>
+        Price: <strong>USD: {props.price.toFixed(2)}</strong>
+      </p>
     </div>
   );
 };
