612444ace0995bf1a1d053d20f067ddbb913f668
vjpal3 <vrishalipal@yahoo.com>
2020-02-06 20:37:10 -0500

    Set up a JS Config for the form

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -3,15 +3,33 @@ import classes from './Input.module.css';
 
 const input = props => {
   let inputElement;
-  switch (props.inputtype) {
+  switch (props.elementType) {
     case 'input':
-      inputElement = <input {...props} className={classes.InputElement} />;
+      inputElement = (
+        <input
+          className={classes.InputElement}
+          {...props.elementConfig}
+          value={props.value}
+        />
+      );
       break;
     case 'textarea':
-      inputElement = <textarea {...props} className={classes.InputElement} />;
+      inputElement = (
+        <textarea
+          className={classes.InputElement}
+          {...props.elementConfig}
+          value={props.value}
+        />
+      );
       break;
     default:
-      inputElement = <input {...props} className={classes.InputElement} />;
+      inputElement = (
+        <input
+          className={classes.InputElement}
+          {...props.elementConfig}
+          value={props.value}
+        />
+      );
   }
   return (
     <div className={classes.Input}>
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,11 +7,56 @@ import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
   state = {
-    name: '',
-    email: '',
-    address: {
-      street: '',
-      postalCode: ''
+    orderForm: {
+      name: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Your Name'
+        },
+        value: ''
+      },
+      street: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Street'
+        },
+        value: ''
+      },
+      zipCode: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Zip Code'
+        },
+        value: ''
+      },
+      country: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Country'
+        },
+        value: ''
+      },
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'email',
+          placeholder: 'Your Email'
+        },
+        value: ''
+      },
+      deliveryMethod: {
+        elementType: 'select',
+        elementConfig: {
+          options: [
+            { value: 'fastest', displayValue: 'Fastest' },
+            { value: 'cheapest', displayValue: 'Cheapest' }
+          ]
+        }
+      }
     },
     loading: false
   };
@@ -21,17 +66,7 @@ class ContactData extends Component {
     this.setState({ loading: true });
     const order = {
       ingredients: this.props.ingredients,
-      price: this.props.price,
-      customer: {
-        name: 'Vrishali Pal',
-        address: {
-          street: 'TestStreet 1',
-          zipCode: '01234',
-          country: 'USA',
-          email: 'test@test.com'
-        },
-        deliveryMethod: 'fastest'
-      }
+      price: this.props.price
     };
     axios
       .post('/orders.json', order)
@@ -47,12 +82,7 @@ class ContactData extends Component {
   render() {
     let form = (
       <form>
-        <Input
-          inputtype='input'
-          type='text'
-          name='name'
-          placeholder='Your Name'
-        />
+        <Input elementType='' elementConfig='' value='' />
         <Input
           inputtype='input'
           type='text'
